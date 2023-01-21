ec1e5d4353f0185b9a1827d1f13be199cd045320
AviSharma01 <avisharma1204@gmail.com>
2023-01-21 13:39:54 -0800

    Changes to authentication.js

--- backend/routers/authentication.js
+++ backend/routers/authentication.js
@@ -5,23 +5,25 @@ var db = require('../db');
 
 
 passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  clientID: process.env['GOOGLE_CLIENT_ID'], //TO BE ADDED
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'], //TO BE ADDED
   callbackURL: '/oauth2/redirect/google',
   scope: [ 'profile' ]
 }, function verify(issuer, profile, cb) {
-    //Finding the users 
+    //Finding the existing users from the credentials table created.
   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
     issuer,
     profile.id
   ], function(err, row) {
     if (err) { return cb(err); }
     if (!row) {
+        // If the user does not exist, add to the database.
       db.run('INSERT INTO users (name) VALUES (?)', [
         profile.displayName
       ], function(err) {
         if (err) { return cb(err); }
         var id = this.lastID;
+        // Add into the credentials table.
         db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
           id,
           issuer,
@@ -69,22 +71,7 @@ router.get('/login', function(req, res, next) {
 /* GET /login/federated/accounts.google.com
  *
  * This route redirects the user to Google, where they will authenticate.
- *
- * Signing in with Google is implemented using OAuth 2.0.  This route initiates
- * an OAuth 2.0 flow by redirecting the user to Google's identity server at
- * 'https://accounts.google.com'.  Once there, Google will authenticate the user
- * and obtain their consent to release identity information to this app.
- *
- * Once Google has completed their interaction with the user, the user will be
- * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
- */
 router.get('/login/federated/google', passport.authenticate('google'));
-
-/*
-    This route completes the authentication sequence when Google redirects the
-    user back to the application.  When a new user signs in, a user account is
-    automatically created and their Google account is linked.  When an existing
-    user returns, they are signed in to their linked account.
 */
 router.get('/oauth2/redirect/google', passport.authenticate('google', {
   successReturnToOrRedirect: '/',
