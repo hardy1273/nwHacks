0fed09d0c4fb8eaf3a89782414d3852eb8f437bb
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2023-01-21 20:39:39 -0800

    card changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,9 +9,9 @@ function Navbar({searchTerm, setSearchTerm, user}) {
   
   return (
     <div class="flex flex-row p-50">
-      <nav class="navbar bg-body-tertiary flex-row p-2 rounded-right mt-10 " style={{ outline : "none" , borderRadius: "50px"}}>
-      <div class="container-fluid flex flex-row rounded-2">
-        <form class="d-flex flex-row flex-10 w-100 rounded-right" role="search" styles={{userSelect: "none"}}>
+      <nav class="navbar bg-body-tertiary flex-row p-2 rounded-right mt-16 " style={{ outline : "none" }}>
+      <div class="container-fluid flex flex-row rounded-2 border-rounded">
+        <form class="d-flex flex-row flex-10 w-100 border rounded-border" role="search" styles={{userSelect: "none", border: 0 }}>
           <input class="form-control flex-6 me-2" type="search" placeholder="Search" aria-label="Search"
           onChange={(e) => setSearchTerm(e.target.value)}
           value={searchTerm}
