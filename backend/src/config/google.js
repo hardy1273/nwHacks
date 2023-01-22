35349586df36a396ebaaafa5de362cdacd26c145
chrisamrl <88105670+chrisamrl@users.noreply.github.com>
2023-01-21 16:12:41 -0800

    more commit

--- backend/src/config/google.js
+++ backend/src/config/google.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const User = require("../user/user.model");
+const UserService = require("../user");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
@@ -10,7 +10,37 @@ passport.use(
       clientSecret: process.env.CLIENT_SECRET,
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log("user profile is: ", profile);
+      const id = profile.id;
+      const email = profile.emails[0].value;
+      const firstName = profile.name.givenName;
+      const lastName = profile.name.familyName;
+      const profilePhoto = profile.photos[0].value;
+      const source = "google";
+
+      const currentUser = await UserService.getUserByEmail({
+        email,
+      });
+
+      if (!currentUser) {
+        const newUser = await UserService.addGoogleUser({
+          id,
+          email,
+          firstName,
+          lastName,
+          profilePhoto,
+        });
+        return done(null, newUser);
+      }
+
+      // if (currentUser.source != "google") {
+      //   //return error
+      //   return done(null, false, {
+      //     message: `You have previously signed up with a different signin method`,
+      //   });
+      // }
+
+      currentUser.lastVisited = new Date();
+      return done(null, currentUser);
     }
   )
 );
