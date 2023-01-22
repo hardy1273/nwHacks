4cc6c7f477a58f8deffade549aa3fc1fe5ca1f88
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2023-01-21 20:27:18 -0800

    card changes

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 //style="width: 18rem"
 const EventCard = ({name, description, image}) => {
   return (
-    <div class="card" >
-    <img class="card-img-top" src="https://static.toiimg.com/thumb/msid-95743162,width-1280,height-720,resizemode-4/.jpg" alt="Card image cap"/>
+    <div class="card" style={{borderRadius:"5%"}} >
+    <img class="card-img-top" src="https://static.toiimg.com/thumb/msid-95743162,width-1280,height-720,resizemode-4/.jpg" alt="Card image cap" style={{padding:"20px", borderRadius:"30px"}}/>
     <div class="card-body">
     <h5 class="card-title">{name}</h5>
     <p class="card-text">{description}</p>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,9 +9,9 @@ function Navbar({searchTerm, setSearchTerm, user}) {
   
   return (
     <div class="flex flex-row p-50">
-      <nav class="navbar bg-body-tertiary flex-row p-2  mt-10 " style={{outline : "none" , borderRadius: "50px"}}>
+      <nav class="navbar bg-body-tertiary flex-row p-2 rounded-right mt-10 " style={{ outline : "none" , borderRadius: "50px"}}>
       <div class="container-fluid flex flex-row rounded-2">
-        <form class="d-flex flex-row flex-10 w-100" role="search">
+        <form class="d-flex flex-row flex-10 w-100 rounded-right" role="search" styles={{userSelect: "none"}}>
           <input class="form-control flex-6 me-2" type="search" placeholder="Search" aria-label="Search"
           onChange={(e) => setSearchTerm(e.target.value)}
           value={searchTerm}
